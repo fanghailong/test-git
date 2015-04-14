cdca89829c8e02070bd9bd1ae0ca2091c72fec25
fanghailong <HaiLong.Fang@Runmit.com>
2015-04-14 16:28:43 +0800

    测试GIT

---
+++ test-git/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test-git
+//
+//  Created by fanghailong on 15-4-14.
+//  Copyright (c) 2015年 superd3d. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
